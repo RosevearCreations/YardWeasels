f8be45a93841e11e2665f98e6f8fb4a2fbdba3a4
RosevearCreations <jfrosevear@gmail.com>
2026-02-02 17:47:53 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+const CACHE = 'ywi-hse-v1';
+const ASSETS = ['/', '/index.html', '/style.css', '/app.js', '/public/manifest.json'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy));
+      return res;
+    }))
+  );
+});
